e974889ccd71295c8009b867b12932368a1cebd8
mattkhan <mattkhan@users.noreply.github.com>
2018-11-23 21:13:31 -0800

    Update home

--- src/routes/pages/Home/Home.js
+++ src/routes/pages/Home/Home.js
@@ -39,8 +39,8 @@ export const Home = ({ loggedIn }) => {
       <div style={{ margin: '0', paddingTop: '2em', fontWeight: '600', fontSize: '4em' }}>Momentum</div>
       <div style={{ color: 'white', fontSize: '1.7em', marginBottom: '2em' }}>Find storage providers near you.</div>
       <div style={{ width: '400px', padding: '20px', borderRadius: '0.5rem', backgroundColor: '#242729', fontSize: '1.2em', lineHieght: '1.2em', marginBottom: '1em' }}>
-        Ever wanted to store something locally, but all those solutions were too expensive? Momentum will match you with
-        storage providers in your area and give you a cheap and convenient storage solution.
+        Ever wanted to store something locally, but the solutions you found were too expensive? Momentum will match you with
+        storage providers in your area at a reasonable price.
       </div>
 
       <ButtonGrid>
