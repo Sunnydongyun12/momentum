7c69f11dd0098cb93225d584dff548bbe9757411
mattkhan <mattkhan@users.noreply.github.com>
2018-11-29 22:53:20 -0800

    Make providers load on first render

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,11 @@
-import * as firebase from "firebase";
+import * as firebase from 'firebase';
 const FirebaseConfig = {
-  apiKey: "AIzaSyCrzsnSKjkBYmK620Qt8gjlPCgDZGtdY-g",
-  authDomain: "cs188momentum.firebaseapp.com",
-  databaseURL: "https://cs188momentum.firebaseio.com",
-  projectId: "cs188momentum",
-  storageBucket: "cs188momentum.appspot.com",
-  messagingSenderId: "188044312099"
+  apiKey: 'AIzaSyCrzsnSKjkBYmK620Qt8gjlPCgDZGtdY-g',
+  authDomain: 'cs188momentum.firebaseapp.com',
+  databaseURL: 'https://cs188momentum.firebaseio.com',
+  projectId: 'cs188momentum',
+  storageBucket: 'cs188momentum.appspot.com',
+  messagingSenderId: '188044312099',
 };
 
 firebase.initializeApp(FirebaseConfig);
--- src/redux/providers/actions/index.js
+++ src/redux/providers/actions/index.js
@@ -1,34 +1,32 @@
-import { databaseRef } from "../../../config/firebase";
-export const SELECT_PROVIDER = "SELECT_PROVIDER";
-export const LOAD_PROVIDERS_REQUEST = "LOAD_PROVIDERS_REQUEST";
-export const LOAD_PROVIDERS_SUCCESS = "LOAD_PROVIDERS_SUCCESS";
-
-var providers = {};
+import { databaseRef } from '../../../config/firebase';
+export const SELECT_PROVIDER = 'SELECT_PROVIDER';
+export const LOAD_PROVIDERS_REQUEST = 'LOAD_PROVIDERS_REQUEST';
+export const LOAD_PROVIDERS_SUCCESS = 'LOAD_PROVIDERS_SUCCESS';
 
 export const selectProvider = provider => ({
   type: SELECT_PROVIDER,
-  provider
+  provider,
 });
 
 export const loadProvidersRequest = () => ({
-  type: LOAD_PROVIDERS_REQUEST
+  type: LOAD_PROVIDERS_REQUEST,
 });
 
 export const loadProvidersSucess = json => ({
   type: LOAD_PROVIDERS_SUCCESS,
-  providers: json
+  providers: json,
 });
 
 export const fetchProviders = () => dispatch => {
-  databaseRef.child("providers").on("value", function(snapshot) {
+  let providers = {};
+  dispatch(loadProvidersRequest());
+
+  databaseRef.child('providers').on('value', function(snapshot) {
     snapshot.forEach(function(childSnapshot) {
       var childKey = childSnapshot.key;
       var childData = childSnapshot.val();
       providers[childKey] = childData;
     });
+    dispatch(loadProvidersSucess(providers));
   });
-
-  dispatch(loadProvidersRequest());
-
-  return dispatch(loadProvidersSucess(providers));
 };
